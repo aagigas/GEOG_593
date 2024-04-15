30ee7a313408fb0c78160ab91044238e4159110b
aagigas <91869878+aagigas@users.noreply.github.com>
2024-04-15 10:45:11 -0700

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,21 @@
+function toggle(targetID) {
+    if (document.getElementById) {
+        target = document.getElementById(targetID);
+        if (target.style.display == "none") {
+            target.style.display = "";
+        } else {
+            target.style.display = "none"
+        }
+    }
+}
+
+function popup(mylink, windowname, width, height) {
+    if (!window.focus) return true;
+    var href;
+    if (typeof (mylink) == 'string')
+        href = mylink;
+    else
+        href = mylink.href;
+    window.open(href, windowname, "type=fullWindow,scrollbars=yes");
+    return false;
+}
